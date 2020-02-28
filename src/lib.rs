204642c933e1b5fd70eec1519d84a56ad43fee0f
Mark LeMoine <linclelinkpart5@gmail.com>
2020-02-28 11:00:25 -0800

    Simple usage of `anagma` crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,19 @@
+use anagma::metadata::stream::value::ValueStream;
+use anagma::metadata::stream::Error as StreamError;
+use anagma::metadata::value::Value;
+
+pub enum UnaryOp {
+    Count,
+    First,
+    Last,
+}
+
+impl UnaryOp {
+    pub fn process(&self, value_stream: ValueStream) -> Result<Value, StreamError> {
+        Ok(Value::Null)
+    }
+}
+
 #[cfg(test)]
 mod tests {
 }
